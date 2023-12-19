63054e18560e605c2d62f140809efad50617a4aa
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-12-19 22:46:26 +0000

    add node insertion methods

--- Program.cs
+++ Program.cs
@@ -4,16 +4,22 @@ int choice, data, k, x;
 
 var list = new SingleLinkedList();
 
+list.CreateList();
+
 while (true)
 {
     Console.WriteLine("1. Display list");
     Console.WriteLine("2. Count the number of nodes");
-    Console.WriteLine("3. Quit");
+    Console.WriteLine("3. Search for an element");
+    Console.WriteLine("4. Insert a node in empty list/ in the beginning of list");
+    Console.WriteLine("5. Insert a node at the end of list");
+
+    Console.WriteLine("6. Quit");
 
     Console.Write("Enter your choice: ");
     choice = int.Parse(Console.ReadLine());
 
-    if (choice == 3)
+    if (choice == 6)
         break;
 
     switch (choice)
--- SingleLinkedList.cs
+++ SingleLinkedList.cs
@@ -10,7 +10,59 @@ namespace D2_challenge
 			_start = null;
 		}
 
-		public void DisplayList()
+        // node insertion
+        public void InsertInBeginning(int data)
+        {
+            Node temp = new Node(data);
+            temp.next = _start;
+            _start = temp;
+
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node p;
+            Node temp = new Node(data);
+
+
+        if (_start == null)
+            {
+                _start = temp;
+                return;
+            };
+
+            p = _start;
+            while (p.next != null)
+            {
+                p = p.next; //traverse & reference the last node
+
+            }
+
+            p.next = temp;
+        }
+
+
+        public void CreateList()
+        {
+            int i, n, data;
+
+            Console.Write("Enter the number of nodes : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+                return;
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("Enter the element to be inserted : ");
+                data = Convert.ToInt32(Console.ReadLine());
+                InsertAtEnd(data);
+            }
+        }
+
+
+
+        public void DisplayList()
 		{
 
 		}
