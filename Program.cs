e8e66b3210d393b0c05e1a2c1cc186aa10c5edd5
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-12-19 21:29:30 +0000

    create the main program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using D2_challenge;
+
+int choice, data, k, x;
+
+var list = new SingleLinkedList();
+
+while (true)
+{
+    Console.WriteLine("1. Display list");
+    Console.WriteLine("2. Count the number of nodes");
+    Console.WriteLine("3. Quit");
+
+    Console.Write("Enter your choice: ");
+    choice = int.Parse(Console.ReadLine());
+
+    if (choice == 3)
+        break;
+
+    switch (choice)
+    {
+        case 1:
+            list.DisplayList();
+            break;
+
+        case 2:
+            list.CountNodes();
+            break;
+
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Exiting...");
+
+}
+
---
+++ SingleLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace D2_challenge
+{
+	public class SingleLinkedList
+	{
+		private Node _start;
+
+		public SingleLinkedList()
+		{
+			_start = null;
+		}
+
+		public void DisplayList()
+		{
+
+		}
+
+        public int CountNodes()
+		{
+			return 0;
+		}
+
+
+    }
+}
+
