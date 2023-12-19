11ac574cd7695081a1003aff80288c1b3e3f6c6a
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-12-19 20:55:51 +0000

    use class to create Node

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace D2_challenge
+{
+	public class Node
+	{
+		public int element { get; set; }
+		public Node next { get; set; }
+
+		public Node(int i)
+		{
+			element = i;
+			next = null;
+		}
+	}
+}
+
