0f70dcdcc1264064e607065a4de7b06ae24cf756
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-12-22 21:18:37 +0000

    Leet code - Merge two sorted linked list

---
+++ ListNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace D2_challenge
+{
+
+	// defintion of singly-linked list
+	public class ListNode
+	{
+		public int val { get; set; }
+		public ListNode next { get; set; }
+
+		public ListNode(int val=0, ListNode next=null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+
+
+		public  static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+		{
+			if (l1 is null)
+				return l2;
+
+			if (l2 is null)
+			{
+				return l1;
+			}
+
+			if (l1.val>l2.val)
+			{
+				l2.next = MergeTwoLists(l1, l2.next);
+				return l2;
+			}
+			else
+			{
+                l1.next = MergeTwoLists(l1.next, l2);
+                return l1;
+            }
+
+		}
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,118 +1,131 @@
 ﻿using D2_challenge;
 
-int choice, data, k, x;
+//int choice, data, k, x;
 
-var list = new SingleLinkedList();
+//var list = new SingleLinkedList();
 
-list.CreateList();
+//list.CreateList();
 
-while (true)
+//while (true)
+//{
+//    Console.WriteLine("1. Display list");
+//    Console.WriteLine("2. Count the number of nodes");
+//    Console.WriteLine("3. Search for an element");
+//    Console.WriteLine("4. Insert a node in empty list/ in the beginning of list");
+//    Console.WriteLine("5. Insert a node at the end of list");
+//    Console.WriteLine("6. Insert a node after a specified node");
+//    Console.WriteLine("7. Insert a node before a specified node");
+//    Console.WriteLine("8. Insert a node at a specified position");
+//    Console.WriteLine("9.Delete the first node");
+//    Console.WriteLine("10.Delete the last node");
+//    Console.WriteLine("11.Delete any given node");
+
+
+
+
+
+//    Console.WriteLine("999. Quit");
+
+//    Console.Write("Enter your choice: ");
+//    choice = int.Parse(Console.ReadLine());
+
+//    if (choice == 999)
+//        break;
+
+//    switch (choice)
+//    {
+//        case 1:
+//            list.DisplayList();
+//            break;
+
+//        case 2:
+//            list.CountNodes();
+//            break;
+
+//        case 3:
+//            Console.Write("Enter the element to be searched: ");
+//            data = int.Parse(Console.ReadLine());
+//            list.Search(data);
+//            break;
+
+//        case 4:
+//            Console.Write("Enter the element to be inserted: ");
+//            data = int.Parse(Console.ReadLine());
+//            list.InsertInBeginning(data);
+//            break;
+
+//        case 5:
+//            Console.Write("Enter the element to be inserted: ");
+//            data = int.Parse(Console.ReadLine());
+//            list.InsertAtEnd(data);
+//            break;
+
+//        case 6:
+//            Console.Write("Enter the element to be inserted: ");
+//            data = int.Parse(Console.ReadLine());
+
+//            Console.Write("Enter the specified node: ");
+//            x = int.Parse(Console.ReadLine());
+
+//            list.InsertAfter(data, x);
+//            break;
+
+//        case 7:
+//            Console.Write("Enter the element to be inserted: ");
+//            data = int.Parse(Console.ReadLine());
+
+//            Console.Write("Enter the specified node: ");
+//            x = int.Parse(Console.ReadLine());
+
+//            list.InsertBefore(data, x);
+//            break;
+
+//        case 8:
+//            Console.Write("Enter the element to be inserted: ");
+//            data = int.Parse(Console.ReadLine());
+
+//            Console.Write("Enter the specified position: ");
+//            k = int.Parse(Console.ReadLine());
+
+//            list.InsertAtPosition(data,k);
+//            break;
+
+//        case 9:
+//            list.DeleteFirstNode();
+//            break;
+
+//        case 10:
+//            list.DeleteLastNode();
+//            break;
+
+//        case 11:
+//            Console.Write("Enter the node to be deleted: ");
+//            data = int.Parse(Console.ReadLine());
+
+
+//            list.DeleteNode(data);
+//            break;
+
+//        default:
+//            Console.WriteLine("Invalid choice");
+//            break;
+//    }
+
+//    Console.WriteLine();
+//    Console.WriteLine("Exiting...");
+
+//}
+
+ListNode l1 = new ListNode(1);
+l1.next = new ListNode(2);
+l1.next.next = new ListNode(4);
+
+ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+var l3 = ListNode.MergeTwoLists(l1, l2);
+
+while (l3!=null)
 {
-    Console.WriteLine("1. Display list");
-    Console.WriteLine("2. Count the number of nodes");
-    Console.WriteLine("3. Search for an element");
-    Console.WriteLine("4. Insert a node in empty list/ in the beginning of list");
-    Console.WriteLine("5. Insert a node at the end of list");
-    Console.WriteLine("6. Insert a node after a specified node");
-    Console.WriteLine("7. Insert a node before a specified node");
-    Console.WriteLine("8. Insert a node at a specified position");
-    Console.WriteLine("9.Delete the first node");
-    Console.WriteLine("10.Delete the last node");
-    Console.WriteLine("11.Delete any given node");
-
-
-
-
-
-    Console.WriteLine("999. Quit");
-
-    Console.Write("Enter your choice: ");
-    choice = int.Parse(Console.ReadLine());
-
-    if (choice == 999)
-        break;
-
-    switch (choice)
-    {
-        case 1:
-            list.DisplayList();
-            break;
-
-        case 2:
-            list.CountNodes();
-            break;
-
-        case 3:
-            Console.Write("Enter the element to be searched: ");
-            data = int.Parse(Console.ReadLine());
-            list.Search(data);
-            break;
-
-        case 4:
-            Console.Write("Enter the element to be inserted: ");
-            data = int.Parse(Console.ReadLine());
-            list.InsertInBeginning(data);
-            break;
-
-        case 5:
-            Console.Write("Enter the element to be inserted: ");
-            data = int.Parse(Console.ReadLine());
-            list.InsertAtEnd(data);
-            break;
-
-        case 6:
-            Console.Write("Enter the element to be inserted: ");
-            data = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter the specified node: ");
-            x = int.Parse(Console.ReadLine());
-
-            list.InsertAfter(data, x);
-            break;
-
-        case 7:
-            Console.Write("Enter the element to be inserted: ");
-            data = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter the specified node: ");
-            x = int.Parse(Console.ReadLine());
-
-            list.InsertBefore(data, x);
-            break;
-
-        case 8:
-            Console.Write("Enter the element to be inserted: ");
-            data = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter the specified position: ");
-            k = int.Parse(Console.ReadLine());
-
-            list.InsertAtPosition(data,k);
-            break;
-
-        case 9:
-            list.DeleteFirstNode();
-            break;
-
-        case 10:
-            list.DeleteLastNode();
-            break;
-
-        case 11:
-            Console.Write("Enter the node to be deleted: ");
-            data = int.Parse(Console.ReadLine());
-
-
-            list.DeleteNode(data);
-            break;
-
-        default:
-            Console.WriteLine("Invalid choice");
-            break;
-    }
-
-    Console.WriteLine();
-    Console.WriteLine("Exiting...");
-
+    Console.WriteLine(l3.val);
+    l3 = l3.next;
 }
-
